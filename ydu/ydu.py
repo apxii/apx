51fcc23edc5ab390a8a37a33cd6a122085aeea2d
Андрей Панов <xn--41a@xn----7sbbihg2a3afkii.xn--p1ai>
2019-10-14 22:49:27 +0300

    PDD API2

--- ydu/ydu.py
+++ ydu/ydu.py
@@ -5,62 +5,92 @@
 Обновление данных в DNS на Яндексе
 '''
 
+import urllib
 import urllib2
-import xml.etree.ElementTree as et
 import argparse
+import json
 
 def YandexGetDomainRecords(token,domain):
-    url="https://pddimp.yandex.ru/nsapi/get_domain_records.xml?token=%s&domain=%s" % (token,domain)
-    f=urllib2.urlopen(url)
+    headers = {}
+    headers ["PddToken"] = token
+    url = "https://pddimp.yandex.ru/api2/admin/dns/list?domain={0}".format(domain)
+    req=urllib2.Request(url, headers = headers)
+    #print (url)
+    #print (req.headers)
+    f=urllib2.urlopen(req)
+
     return f.read()
 
 def YandexEditARecord(token,domain,subdomain,record_id,address):
-    url="https://pddimp.yandex.ru/nsapi/edit_a_record.xml?token=%s&domain=%s&subdomain=%s&record_id=%s&content=%s" % (token,domain,subdomain,record_id,address)
-    f=urllib2.urlopen(url)
+    headers = {}
+    headers ["PddToken"] = token
+    url="https://pddimp.yandex.ru/api2/admin/dns/edit"
 
-    rt=et.fromstring(f.read())
-    err=rt.find('./domains/error')
-    errtext=err.text
+    values = {}
+    values ["domain"] = domain
+    values ["record_id"] = record_id
+    values ["subdomain"] = subdomain
+    values ["content"] = address
+
+    data = urllib.urlencode(values)
+    req=urllib2.Request(url, data, headers)
+
+    f=urllib2.urlopen(req)
+
+    rt=json.loads(f.read())
+    print(rt)
+    errtext=rt["success"]
     if errtext!='ok':
-	print "Error updating %s: %s" % (subdomain, errtext)
-	return 1
+        print ("Error updating %s: %s" % (subdomain, errtext))
+        return 1
 
     return 0
 
 def YandexAddARecord(token,domain,subdomain,address):
-    url="https://pddimp.yandex.ru/nsapi/add_a_record.xml?token=%s&domain=%s&subdomain=%s&content=%s" % (token,domain,subdomain,address)
-    f=urllib2.urlopen(url)
+    headers = {}
+    headers ["PddToken"] = token
+    url="https://pddimp.yandex.ru/api2/admin/dns/add"
 
-    rt=et.fromstring(f.read())
-    err=rt.find('./domains/error')
-    errtext=err.text
+    values = {}
+    values ["domain"] = domain
+    values ["type"] = "A"
+    values ["subdomain"] = subdomain
+    values ["content"] = address
+
+    data = urllib.urlencode(values)
+    req=urllib2.Request(url, data, headers)
+
+    f=urllib2.urlopen(req)
+
+    rt=json.loads(f.read())
+    print(rt)
+    errtext=rt["success"]
     if errtext!='ok':
-	print "Error adding %s: %s" % (subdomain, errtext)
-	return 1
+        print ("Error updating %s: %s" % (subdomain, errtext))
+        return 1
 
     return 0
 
-def YandexDomainRecordsUpdate(token,domain,rxml,subdomain,address):
-    root=et.fromstring(rxml)
-    err=root.find('./domains/error')
-    errtext=err.text
-#    print "Response error is '%s'" % errtext
+def YandexDomainRecordsUpdate(token,domain,rt,subdomain,address):
+    root=json.loads(rt)
+    #print(root)
+    errtext=root["success"]
+    #print "Response error is '%s'" % errtext
     if errtext!='ok': return 1
 
     ret=-1
-    for dmn in root.findall("./domains/domain/response/record[@subdomain='%s']" % subdomain):
-	if dmn.get("type")=='A':
-#	    print dmn.tag,dmn.attrib,dmn.text
-#	    print dmn.get("id"), dmn.get("subdomain"), dmn.get("type")
-	    record_id=dmn.get("id")
-	    if dmn.text!=address:
-		ret=YandexEditARecord(token,domain,subdomain,record_id,address)
-	    else:
-		print "Address of %s is the same (%s), not updating" % (subdomain,address)
-		ret=0
+    for dmn in root["records"]:
+        if dmn["type"]=='A' and dmn["subdomain"]==subdomain:
+            record_id=dmn["record_id"]
+            if dmn["content"]!=address:
+                ret=YandexEditARecord(token,domain,subdomain,record_id,address)
+            else:
+                print "Address of %s is the same (%s), not updating" % (subdomain,address)
+                ret=0
 
+    return 0
     if ret==-1:
-	ret=YandexAddARecord(token,domain,subdomain,address)
+        ret=YandexAddARecord(token,domain,subdomain,address)
 
     return ret
 
@@ -79,6 +109,5 @@ args=parser.parse_args()
 if (args.token==None) or (args.domain==None) or (args.host==None) or (args.address==None) :
     parser.print_help()
 else:
-    rxml=YandexGetDomainRecords(args.token,args.domain)
-    #print rxml
-    YandexDomainRecordsUpdate(args.token,args.domain,rxml,args.host,args.address)
+    rt=YandexGetDomainRecords(args.token,args.domain)
+    YandexDomainRecordsUpdate(args.token,args.domain,rt,args.host,args.address)
